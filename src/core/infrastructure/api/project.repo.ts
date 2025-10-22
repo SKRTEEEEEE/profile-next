854593a1fe0e6338aa85ebf51b5aeac958c3cc72
SKRTEEEEEE <125660042+SKRTEEEEEE@users.noreply.github.com>
2025-10-22 21:56:41 +0200

    Update project.repo.ts

--- src/core/infrastructure/api/project.repo.ts
+++ src/core/infrastructure/api/project.repo.ts
@@ -26,5 +26,5 @@ implements ProjectInterface{
 
 //singleton
 // 🔥 FORZADO A LOCALHOST PARA DEBUGGING
-export const projectApiRepository = new ProjectApiRepository("http://localhost:3001")
-// export const projectApiRepository = new ProjectApiRepository(process.env.TEST_ENV !== "development" ? "https://kind-creation-production.up.railway.app" : "http://localhost:3001")
+// export const projectApiRepository = new ProjectApiRepository("http://localhost:3001")
+export const projectApiRepository = new ProjectApiRepository(process.env.TEST_ENV !== "development" ? "https://kind-creation-production.up.railway.app" : "http://localhost:3001")
